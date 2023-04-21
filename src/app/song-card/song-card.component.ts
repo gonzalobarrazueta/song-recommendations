f9de524ca0781270e0ec41239c977b82599ed03a
Gonzalo Barrazueta <u20191a972@upc.edu.pe>
2023-04-20 21:36:44 -0500

    Add @Input decorator

--- src/app/song-card/song-card.component.ts
+++ src/app/song-card/song-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Track} from "../models/track";
 
 @Component({
   selector: 'app-song-card',
@@ -7,18 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SongCardComponent implements OnInit {
 
-  song_title: string;
-  artist_name: string;
-  img_url: string;
-  profile_photo_url: string;
-  gradient: string;
-  border_color: string;
+  @Input() song!: Track;
+  song_title: string = '';
+  artist_name: string = '';
+  img_url: string = '';
+  profile_photo_url: string = '';
+  gradient: string = '';
+  border_color: string = '';
 
   constructor() {
-    this.song_title = 'Good Night Good Dream';
-    this.artist_name = 'Nerd Connection';
-    this.img_url = 'https://m.media-amazon.com/images/I/51vAEyKxOiL._UXNaN_FMjpg_QL85_.jpg';
-    this.profile_photo_url = 'https://i.scdn.co/image/ab6761610000e5ebcd016d0a0783a321312e1733';
+    this.song_title = this.song.name;
+    this.artist_name = this.song.artist.name;
+    this.img_url = this.song.img;
+    this.profile_photo_url = this.song.artist.img;
     this.gradient = 'background: linear-gradient(180deg, #EFDFCF 0%, #F7F2E3 56.68%, rgba(255, 255, 255, 0.9) 100%);';
     this.border_color = 'border-color: #F7F2E3;';
   }
