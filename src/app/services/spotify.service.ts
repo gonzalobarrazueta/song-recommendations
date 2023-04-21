979943db9452c54584fec0d5219fb21ee84162e8
Gonzalo Barrazueta <u20191a972@upc.edu.pe>
2023-04-20 20:18:08 -0500

    Add query parameter

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -12,7 +12,7 @@ export class SpotifyService {
 
   getTopItems(access_token: string, type: string) {
     let headers = new HttpHeaders().set('Authorization', `Bearer ${access_token}`);
-    return this.http.get(`${base_address}/me/top/${type}?limit=4`, {headers});
+    return this.http.get(`${base_address}/me/top/${type}?time_range=short_term&limit=4`, {headers});
   }
 
   getArtistById(access_token: string, id: string) {
