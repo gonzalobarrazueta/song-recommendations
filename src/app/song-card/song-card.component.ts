04418f0bc7e8e41554914ac5b34f2a6f3bcc96da
Gonzalo Barrazueta <u20191a972@upc.edu.pe>
2023-04-20 22:00:46 -0500

    Change and delete variables

--- src/app/song-card/song-card.component.ts
+++ src/app/song-card/song-card.component.ts
@@ -9,18 +9,10 @@ import {Track} from "../models/track";
 export class SongCardComponent implements OnInit {
 
   @Input() song!: Track;
-  song_title: string = '';
-  artist_name: string = '';
-  img_url: string = '';
-  profile_photo_url: string = '';
   gradient: string = '';
   border_color: string = '';
 
   constructor() {
-    this.song_title = this.song.name;
-    this.artist_name = this.song.artist.name;
-    this.img_url = this.song.img;
-    this.profile_photo_url = this.song.artist.img;
     this.gradient = 'background: linear-gradient(180deg, #EFDFCF 0%, #F7F2E3 56.68%, rgba(255, 255, 255, 0.9) 100%);';
     this.border_color = 'border-color: #F7F2E3;';
   }
