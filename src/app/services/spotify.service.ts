dd18b19170c56704fc8a7530b23c9cb057fc84a7
Gonzalo Barrazueta <u20191a972@upc.edu.pe>
2023-07-12 18:19:35 -0500

    Refactor getTopItems method to use the Fetch API

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -10,9 +10,16 @@ export class SpotifyService {
 
   constructor(private http: HttpClient) { }
 
-  getTopItems(access_token: string, type: string) {
-    let headers = new HttpHeaders().set('Authorization', `Bearer ${access_token}`);
-    return this.http.get(`${base_address}/me/top/${type}?time_range=short_term&limit=4`, {headers});
+  async getTopItems(access_token: string, type: string) {
+    let headers = new Headers();
+    headers.set('Authorization', `Bearer ${access_token}`);
+
+    const requestOptions = {
+      method: "GET",
+      headers: headers
+    }
+
+    return fetch(`${base_address}/me/top/${type}?time_range=short_term&limit=4`, requestOptions);
   }
 
   getArtistById(access_token: string, id: string) {
