0d069bbcae29688e581e77e48268227db6c61be1
Gonzalo Barrazueta <u20191a972@upc.edu.pe>
2023-07-12 12:31:59 -0500

    Create getAuthorization method

--- src/app/auth-callback/auth-callback.component.ts
+++ src/app/auth-callback/auth-callback.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from "@angular/router";
+import { ActivatedRoute } from "@angular/router";
 import { SpotifyAuthService } from "../services/spotify-auth.service";
 import { SpotifyService } from "../services/spotify.service";
 import { Track } from "../models/track";
-import { Observable } from "rxjs";
 
 @Component({
   selector: 'app-auth-callback',
@@ -21,52 +20,24 @@ export class AuthCallbackComponent implements OnInit {
   constructor(private route: ActivatedRoute, private spotifyAuth: SpotifyAuthService, private spotify: SpotifyService) {}
 
   ngOnInit(): void {
-    // Gets auth code and access token
-    const params$: Observable<Params> = this.route.queryParams;
-    params$.subscribe((params) => {
-      this.auth_code = params['code']
-      console.log(`Auth code: ${this.auth_code}`)
-
-      const access_token$ = this.spotifyAuth.getAccessToken(this.auth_code)
-      access_token$.subscribe(response => {
-        this.access_token = response.access_token;
-      });
-    });
+    this.getAuthorization();
+    setTimeout(() => console.log(this.access_token), 1000)
   }
 
-  getTopTracks() {
-    let track: Track;
-    const top_tracks$ = this.spotify.getTopItems(this.access_token, 'tracks');
-
-    top_tracks$.subscribe((response: any) => {
-      for (let _track of response['items']) {
-        track = {
-          name: _track.name,
-          img: _track.album.images[1].url,
-          artist: {
-            id: _track.artists[0].id,
-            name: _track.artists[0].name,
-            img: ''
-          }
+  getAuthorization() {
+    const urlParams = new URLSearchParams(window.location.search);
+    this.auth_code = urlParams.get("code") as string;
+    this.spotifyAuth.getAccessToken(this.auth_code)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Request failed with status " + response.status);
         }
-        this.top_tracks.push(track);
-      }
-    });
-  }
-
-  getTopTracksArtists() {
-    for (let i = 0; i < this.top_tracks.length; i++) {
-      const artist$ = this.spotify.getArtistById(this.access_token, this.top_tracks[i].artist.id);
-
-      artist$.subscribe((artist: any) => {
-        this.top_tracks[i].artist['img'] = artist.images[2].url;
       })
-    }
-  }
-
-  getUsersTopSongs() {
-    this.getTopTracks();
-    this.getTopTracksArtists();
-    console.log(this.top_tracks);
+      .then(data => {
+        this.access_token = data.access_token;
+      })
+      .catch(error => console.error("Error:", error));
   }
 }
