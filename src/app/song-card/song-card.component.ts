3adeb6f05f37e5c6b42aa2a629120887bd297abd
Gonzalo Barrazueta <u20191a972@upc.edu.pe>
2023-07-13 14:20:00 -0500

    Get gradient based on the color of the track's cover photo

--- src/app/song-card/song-card.component.ts
+++ src/app/song-card/song-card.component.ts
@@ -1,5 +1,6 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Track} from "../models/track";
+import { Component, Input, OnInit } from '@angular/core';
+import { Track } from "../models/track";
+import { average } from "color.js";
 
 @Component({
   selector: 'app-song-card',
@@ -13,11 +14,18 @@ export class SongCardComponent implements OnInit {
   border_color: string = '';
 
   constructor() {
-    this.gradient = 'background: linear-gradient(180deg, #EFDFCF 0%, #F7F2E3 56.68%, rgba(255, 255, 255, 0.9) 100%);';
     this.border_color = 'border-color: #F7F2E3;';
   }
 
   ngOnInit(): void {
+    this.getImageColors()
+      .then(averageColor => {
+        this.gradient = `background: radial-gradient(farthest-corner at 1px 1px, #F0F7FF 0%, ${averageColor}  100%);`;
+      })
+      .catch(console.error)
   }
 
+  async getImageColors() {
+    return await average(this.song.img, { format: "hex" })
+  }
 }
