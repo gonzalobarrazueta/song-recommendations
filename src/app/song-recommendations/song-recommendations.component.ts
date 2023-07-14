fb455f7ff0deeef01e2ce9216a34c061905bb274
Gonzalo Barrazueta <u20191a972@upc.edu.pe>
2023-07-14 13:11:06 -0500

    Get song recommendations

--- src/app/song-recommendations/song-recommendations.component.ts
+++ src/app/song-recommendations/song-recommendations.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from "@angular/router";
 import { Track } from "../models/track";
+import { SharedService } from "../services/shared.service";
+import { SpotifyService } from "../services/spotify.service";
 
 @Component({
   selector: 'app-song-recommendations',
@@ -9,13 +10,43 @@ import { Track } from "../models/track";
 })
 export class SongRecommendationsComponent implements OnInit {
 
-  topTracks: Track[] = [];
+  private accessToken: string = "";
+  tracks: Track[] = [];
+  buildTracks: ((track: any) => Track);
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private sharedService: SharedService, private spotifyService: SpotifyService) {
+    this.buildTracks = this.sharedService.buildTracks;
+  }
 
   ngOnInit(): void {
     const urlParams = new URLSearchParams(window.location.search);
-    this.topTracks = JSON.parse(urlParams.get("data") as string);
+    this.tracks = JSON.parse(urlParams.get("data") as string);
+    this.accessToken = this.sharedService.accessToken;
   }
 
+  getRecommendationsPerTrack(track: Track) {
+    this.getRecommendations(track)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Request failed with status " + response.status);
+        }
+      })
+      .then(data => {
+        this.handleRecommendationsResponse(data);
+      })
+      .catch(error => console.error("Error:", error));
+  }
+
+  async getRecommendations(track: Track) {
+    return await this.spotifyService.getRecommendations(this.accessToken, 3, "PE", [track.artist.id], [], [track.trackId])
+  }
+
+  handleRecommendationsResponse(data: any) {
+    let recommendations = data.tracks;
+    for (let track of recommendations) {
+      this.tracks.push(this.buildTracks(track))
+    }
+  }
 }
