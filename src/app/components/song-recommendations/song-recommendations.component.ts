61b1ce6a6c9bd9ea8412cb44f78ac461e7f80568
Gonzalo Barrazueta <u20191a972@upc.edu.pe>
2023-07-16 22:06:48 -0500

    Add methods to retrieve an artist's profile photo when a recommendation has been requested

--- src/app/components/song-recommendations/song-recommendations.component.ts
+++ src/app/components/song-recommendations/song-recommendations.component.ts
@@ -11,12 +11,19 @@ import { SpotifyService } from "../../services/spotify.service";
 export class SongRecommendationsComponent implements OnInit {
 
   private accessToken: string = "";
+  private artistsPhotos: Map<string, string>;
   tracks: Track[] = [];
-  buildTracks: ((track: any) => Track);
   loading: boolean = false;
+  buildTracks: ((track: any) => Track);
+  mapArtistToPhoto: ((responseArtist: any, artists: Map<string, string>) => Map<string, string>);
 
   constructor(private sharedService: SharedService, private spotifyService: SpotifyService) {
     this.buildTracks = this.sharedService.buildTracks;
+    this.mapArtistToPhoto = this.sharedService.mapArtistToPhoto;
+    this.artistsPhotos = new Map();
+    this.sharedService.artistsProfilePhotos.subscribe((artistsPhotos: Map<string, string>)=> {
+      this.artistsPhotos = artistsPhotos;
+    });
   }
 
   ngOnInit(): void {
@@ -36,8 +43,13 @@ export class SongRecommendationsComponent implements OnInit {
         }
       })
       .then(data => {
-        this.handleRecommendationsResponse(data);
-        this.loading = false;
+        this.handleRecommendationsResponse(data)
+          .then(data => {
+            this.tracks = this.tracks.concat(data);
+          })
+          .then(data => {
+            this.loading = false;
+          });
       })
       .catch(error => {
         this.loading = false;
@@ -49,10 +61,38 @@ export class SongRecommendationsComponent implements OnInit {
     return await this.spotifyService.getRecommendations(this.accessToken, 6, "PE", [track.artist.id], [], [track.trackId])
   }
 
-  handleRecommendationsResponse(data: any) {
+  async handleRecommendationsResponse(data: any) {
     let recommendations = data.tracks;
+    let _recommendations: Track[] = [];
+    let newTrack: Track;
     for (let track of recommendations) {
-      this.tracks.push(this.buildTracks(track))
+      newTrack = this.buildTracks(track);
+      await this.assignArtistToTracks(newTrack)
+        .then(responseTrack => {
+          _recommendations.push(responseTrack);
+        })
+        .catch(error => console.log(error));
     }
+    return _recommendations;
+  }
+
+  async assignArtistToTracks(track: Track): Promise<Track> {
+    await this.spotifyService.getArtistById(this.accessToken, track.artist.id)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Request failed with status " + response.status);
+        }
+      })
+      .then(artist => {
+        this.artistsPhotos = this.mapArtistToPhoto(artist, this.artistsPhotos);
+        track.artist.img = this.artistsPhotos.get(artist.id)!;
+        this.sharedService.artistsProfilePhotos.next(this.artistsPhotos);
+      })
+      .catch(error => {
+        console.error("Error:", error);
+      });
+    return track;
   }
 }
