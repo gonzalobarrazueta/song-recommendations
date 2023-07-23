6e52b14a06282f002927c771c7b540307dcbd174
Gonzalo Barrazueta <u20191a972@upc.edu.pe>
2023-07-22 21:49:10 -0500

    Delete unused method

--- src/app/components/auth-callback/auth-callback.component.ts
+++ src/app/components/auth-callback/auth-callback.component.ts
@@ -14,9 +14,7 @@ export class AuthCallbackComponent implements OnInit {
 
   private auth_code: string = '';
   private access_token: string = '';
-  private responseTracks: Array<any> = [];
   top_tracks: Track[] = [];
-  artistsPhotos: Map<string, string> = new Map();
   buildTracks: ((track: any) => Track);
   loading: boolean = false;
 
@@ -50,39 +48,17 @@ export class AuthCallbackComponent implements OnInit {
     this.loading = true;
     this.spotify.getTopItems(this.access_token, "tracks", 6)
       .then(response => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error("Request failed with status " + response.status);
-        }
+        if (response.ok) return response.json();
+        else throw new Error("Request failed with status " + response.status);
       })
       .then(data => {
-        this.responseTracks = data.items;
-        let tracks = this.responseTracks;
-        for (let i = 0; i < tracks.length; i++) {
-          this.top_tracks.push(this.buildTracks(tracks[i]));
+        for (let i = 0; i < data.items.length; i++) {
+          this.top_tracks.push(this.buildTracks(data.items[i]));
         }
         this.loading = false;
         this.navigateToRecommendationsComponent();
       })
       .catch(error => console.error("Error:", error))
-  }
-
-  async assignArtistToTracks() {
-    for (let i = 0; i < this.top_tracks.length; i++) {
-      try {
-        const response = await this.spotify.getArtistById(this.access_token, this.top_tracks[i].artist.id);
-        if (response.ok) {
-          const responseArtist = await response.json();
-          this.artistsPhotos = this.sharedService.mapArtistToPhoto(responseArtist, this.artistsPhotos);
-          this.top_tracks[i].artist.img = this.artistsPhotos.get(responseArtist.id)!;
-        } else {
-          throw new Error("Request failed with status " + response.status);
-        }
-      } catch (error) {
-        console.error("Error:", error);
-      }
-    }
   }
 
   navigateToRecommendationsComponent() {
